e283d7882cc2c7a6f8ae515071ffc0b2f26fe9c6
diogotrf <diogo.reis.ferreira@ubi.pt>
2023-12-05 19:48:04 +0000

    Pergunta 1 e 2

--- agente.py
+++ agente.py
@@ -5,12 +5,16 @@ criar aqui as funções que respondem às perguntas
 e quaisquer outras que achem necessário criar
 
 colocar aqui os nomes e número de aluno:
-NUM1, NOME1
-NUM2, NOME2
+Diogo Ferreira, 46198
+Afonso Martins, 45838
 
 """
 import time
+nomesM=["Ricardo","Paulo","Adérito","Rui","Miguel","Paulo","Fábio","Carlos","Joaquim"]
+lastVisited=[]
 
+zonas=["teste","montagem","inspeção","escritório","empacotamento","laboratório"]
+lastZone=[]
 
 def work(posicao, bateria, objetos):
     # esta função é invocada em cada ciclo de clock
@@ -19,16 +23,41 @@ def work(posicao, bateria, objetos):
     # posicao = a posição atual do agente, uma lista [X,Y]
     # bateria = valor de energia na bateria, um número inteiro >= 0
     # objetos = o nome do(s) objeto(s) próximos do agente, uma string
-
     # podem achar o tempo atual usando, p.ex.
     # time.time()
-    pass
+	if len(objetos)==1:
+		for nome in nomesM:
+			if nome in objetos[0]:
+				if len(lastVisited)==2:
+					if lastVisited[1]!=nome:
+						lastVisited.pop(0)
+						lastVisited.append(nome)
+				else:
+					lastVisited.append(nome)
+
+
+	if len(objetos)==1:
+		for zona in zonas:
+			if zona in objetos[0]:
+				print("ZONA!")
+				if len(lastZone)==1:
+					lastZone.pop(0)
+					lastZone.append(zona)
+				else:
+					lastZone.append(zona)
 
 def resp1():
-    pass
+	if len(lastVisited)==2:
+		print(lastVisited[0])
+	else:
+		print("Não ha penultima pessoa")
+
 
 def resp2():
-    pass
+	if len(lastZone)==1:
+		print(lastZone[0])
+	else:
+		print("nao ha ultima zona")
 
 def resp3():
     pass
