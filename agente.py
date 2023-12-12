cd67af4e4a6d9a351fa79b0ee406c16dd780963e
diogotrf <diogo.reis.ferreira@ubi.pt>
2023-12-12 17:53:49 +0000

    Pergunta 2 - 100% DONE!

--- agente.py
+++ agente.py
@@ -1,34 +1,28 @@
 """
-DIOGO KINDA THIKKK
 agente.py
 
-criar aqui as funções que respondem às perguntas
-e quaisquer outras que achem necessário criar
-
-colocar aqui os nomes e número de aluno:
 Diogo Ferreira, 46198
 Afonso Martins, 45838
-
 """
 import time
 
 # Corredores
-divisao1 = [(30,165),(135,350)]
-divisao2 = [(165,165),(485,185)]
-divisao3 = [(30,380),(500,435)]
-divisao4 = [(530,215),(635,435)]
+divisao1 = [(30, 165), (135, 350)]
+divisao2 = [(165, 165), (485, 185)]
+divisao3 = [(30, 380), (500, 435)]
+divisao4 = [(530, 215), (635, 435)]
 
 # Divisões
-divisao5 = [(30,30),(135,135)]
-divisao6 = [(530,230),(635,435)]
-divisao7 = [(530,230),(635,435)]
-divisao8 = [(530,230),(635,435)]
-divisao9 = [(530,230),(635,435)]
-divisao10 = [(665,330),(770,385)]
-divisao11 = [(530,230),(635,435)]
-divisao12 = [(530,230),(635,435)]
-divisao13 = [(530,230),(635,435)]
-divisao14 = [(530,230),(635,435)]
+divisao5 = [(30, 30), (135, 135)]
+divisao6 = [(180, 30), (285, 135)]
+divisao7 = [(330, 30), (485, 135)]
+divisao8 = [(530, 30), (770, 185)]
+divisao9 = [(665, 230), (770, 285)]
+divisao10 = [(665, 330), (770, 385)]
+divisao11 = [(530, 465), (770, 570)]
+divisao12 = [(330, 465), (485, 570)]
+divisao13 = [(180, 465), (285, 570)]
+divisao14 = [(30, 465), (135, 570)]
 
 lastVisited = []
 
@@ -36,6 +30,7 @@ zonas = ["teste", "montagem", "inspeção", "escritório", "empacotamento", "lab
 lastZone = []
 
 posicaoGlobal = []
+
 
 def work(posicao, bateria, objetos):
     # esta função é invocada em cada ciclo de clock
@@ -50,8 +45,10 @@ def work(posicao, bateria, objetos):
     global posicaoGlobal
     posicaoGlobal = posicao
 
+    # Pergunta 1
     if len(objetos) == 1:
-        if (objetos[0][-1]!='a') and (("visitante" in objetos[0]) or ("operário" in objetos[0]) or (("supervisor" in objetos[0]))):
+        if (objetos[0][-1] != 'a') and (
+                ("visitante" in objetos[0]) or ("operário" in objetos[0]) or (("supervisor" in objetos[0]))):
             if len(lastVisited) == 2:
                 if lastVisited[1] != objetos[0]:
                     lastVisited.pop(0)
@@ -59,49 +56,63 @@ def work(posicao, bateria, objetos):
             else:
                 lastVisited.append(objetos[0])
 
+    # Pergunta 2
     if len(objetos) == 1:
         for zona in zonas:
             if zona in objetos[0]:
-                if len(lastZone)==1:
+                if len(lastZone) == 1:
                     lastZone.pop(0)
                     lastZone.append(zona)
                 else:
                     lastZone.append(zona)
 
+    if any(start[0] <= posicaoGlobal[0] <= end[0] and start[1] <= posicaoGlobal[1] <= end[1] for start, end in
+           [divisao1, divisao2, divisao3, divisao4]):
+        lastZone.clear()
+
+    # Pergunta 3
+
+
 def resp1():
-    if len(lastVisited)==2:
+    if len(lastVisited) == 2:
         print("A penúltima pessoa foi o " + lastVisited[0] + ".")
     else:
         print("Não há penúltima pessoa.")
 
 
 def resp2():
-    if any(start[0] <= posicaoGlobal[0] <= end[0] and start[1] <= posicaoGlobal[1] <= end[1] for start, end in [divisao1, divisao2, divisao3, divisao4]):
+    if any(start[0] <= posicaoGlobal[0] <= end[0] and start[1] <= posicaoGlobal[1] <= end[1] for start, end in
+           [divisao1, divisao2, divisao3, divisao4]):
         print("Estou no corredor.")
     elif any(start[0] <= posicaoGlobal[0] <= end[0] for start, end in [divisao10]):
         print("Estou na entrada da fábrica.")
-    else:
-        if lastZone:
-            print("Estou na/no " + lastZone[0] + ".")
+    elif any(start[0] <= posicaoGlobal[0] <= end[0] and start[1] <= posicaoGlobal[1] <= end[1] for start, end in
+             [divisao5, divisao6, divisao7, divisao8, divisao9, divisao11, divisao12, divisao13, divisao14]):
+        if len(lastZone) == 1:
+            print("Estou na " + lastZone[0] + ".")
         else:
-            print("Não há última zona.")
-            print(posicaoGlobal)
+            print("Estou numa zona multitarefa.")
 
 
 def resp3():
     pass
 
+
 def resp4():
     pass
+
 
 def resp5():
     pass
 
+
 def resp6():
     pass
 
+
 def resp7():
     pass
+
 
 def resp8():
     pass
